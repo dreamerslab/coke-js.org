cf5a7a08dda2464f8fa98f12ad1f0211e6367e75
ben <ben@dreamerslab.com>
2012-05-18 21:38:58 +0800

    build new version

--- build/js/index.js
+++ build/js/index.js
@@ -1,1 +1,1 @@
-(function(a){a(function(){var b=a("a");b.click(function(){this.blur()}).mousedown(function(){this.blur()}),/msie/i.test(navigator.userAgent)&&!/opera/i.test(navigator.userAgent)&&b.focus(function(){this.blur()})})})(jQuery)
+(function(a,b){a(function(){var b=a("a");b.click(function(){this.blur()}).mousedown(function(){this.blur()}),/msie/i.test(navigator.userAgent)&&!/opera/i.test(navigator.userAgent)&&b.focus(function(){this.blur()})}),a(b).load(prettyPrint)})(jQuery,window)
